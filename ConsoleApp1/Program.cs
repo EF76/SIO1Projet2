cf002bdf2b1bd085555ef5f9963adb5b54d69d8b
EF76 <edith.francois76@gmail.com>
2019-07-09 12:24:45 +0200

    premier programme

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            a = 2;
+            b = 3;
+            c = a + b;
+            Console.WriteLine(" le resultat fait " + c);
+            Console.ReadLine();
+        }
+    }
+}
